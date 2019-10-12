c25c1f8275adfe6cc699909de8893e15cd7617f7
Florian Jüsten <juesten@madeinoffice.com>
2019-10-12 23:17:37 +0200

    poc done

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+import * as fs from "fs"
+import { JiraUrl } from "./jira.url";
+
+
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({ width: 1400, height: 1000 })
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(JiraUrl)
+    mainWindow.webContents.on('did-finish-load', function () {
+        // fs.readFile(__dirname + "/jira.extreme.dark.css", "utf-8", function (error: any, data: any) {
+        fs.readFile(__dirname + "/jira.full.dark.css", "utf-8", function (error: any, data: any) {
+            console.log('data', data)
+            console.log('error', error)
+            if (!error) {
+                var formatedData = data.replace(/\s{2,10}/g, ' ').trim()
+                mainWindow.webContents.insertCSS(formatedData)
+            }
+        })
+    }); 
+    mainWindow.webContents.openDevTools()
+
+}
+app.on('ready', createWindow)
