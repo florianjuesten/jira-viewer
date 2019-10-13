eab9c21d4ac331e4ac36792276951fb96f4fec59
Florian Jüsten <juesten@madeinoffice.com>
2019-10-13 10:13:50 +0200

    style bugs fixed

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import { JiraUrl } from "./jira.url";
 
 function createWindow() {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({ width: 1400, height: 1000 })
-
+    const mainWindow = new BrowserWindow({ width: 1400, height: 1000, backgroundColor: '#333', center: true, titleBarStyle: "customButtonsOnHover", frame: false, thickFrame: true })
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
     // and load the index.html of the app.
     mainWindow.loadURL(JiraUrl)
     mainWindow.webContents.on('did-finish-load', function () {
@@ -17,10 +19,11 @@ function createWindow() {
             if (!error) {
                 var formatedData = data.replace(/\s{2,10}/g, ' ').trim()
                 mainWindow.webContents.insertCSS(formatedData)
+                mainWindow.webContents.executeJavaScript('console.log("test")')
             }
         })
-    }); 
-    mainWindow.webContents.openDevTools()
+    });
+    // mainWindow.webContents.openDevTools()
 
 }
 app.on('ready', createWindow)
